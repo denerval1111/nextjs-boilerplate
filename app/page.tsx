d57f29d233335f5441e0ca0f1d89ab5344deac76
denerval1111 <denerval1111@gmail.com>
2025-05-17 23:45:19 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Header from '../components/Header';
-import HeroSection from '../components/HeroSection';
+
 
 export default function Home() {
   return (
@@ -7,7 +7,7 @@ export default function Home() {
       <Header />
       <main className="pt-24 pb-16">
         <h1 className="text-center text-3xl font-bold mt-8">Desafio Vitalidade</h1>
-      <HeroSection />
+    
       <main>
         {/* Outras seções virão aqui */}
       </main>
