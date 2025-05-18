b83da5304b5222142bc9552c01de4711fb7327a2
denerval1111 <denerval1111@gmail.com>
2025-05-18 00:25:02 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
+import BenefitsSection from '../components/BenefitsSection';
 
 export default function Home() {
   return (
     <>
       <Header />
       <HeroSection />
+      <BenefitsSection />
       <main>
         {/* Outras seções serão adicionadas aqui */}
       </main>
