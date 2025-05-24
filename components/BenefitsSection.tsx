ce13cdec892366f0ac6d450b752470cc69e31eda
denerval1111 <denerval1111@gmail.com>
2025-05-24 14:04:27 -0300

    Update BenefitsSection.tsx

--- components/BenefitsSection.tsx
+++ components/BenefitsSection.tsx
@@ -34,7 +34,7 @@ export default function BenefitsSection( ) {
   ];
 
   return (
-   <section className="bg-white" style={{ marginTop: '-2px' }}>
+   <section className="benefits-section bg-white">
       <div className="container mx-auto px-4">
         <div className="text-center mb-16">
           <h2 className="text-3xl md:text-4xl font-bold mb-4 text-cinza-escuro">
