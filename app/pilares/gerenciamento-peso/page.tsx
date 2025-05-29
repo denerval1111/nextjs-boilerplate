8703cceb30dd087dbdfd22149cb6000b93439d9a
denerval1111 <denerval1111@gmail.com>
2025-05-29 05:14:43 -0300

    Update page.tsx

--- app/pilares/gerenciamento-peso/page.tsx
+++ app/pilares/gerenciamento-peso/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Footer from '@/components/Footer';
+
 
 export default function GerenciamentoPesoPage() {
   return (
@@ -71,7 +71,7 @@ export default function GerenciamentoPesoPage() {
           </div>
         </div>
       </main>
-      <Footer />
+    
     </>
   );
 }
