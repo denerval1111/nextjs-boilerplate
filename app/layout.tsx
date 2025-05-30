d3e64529f6d758738ba0e84cd790422ce0d60bc9
denerval1111 <denerval1111@gmail.com>
2025-05-30 18:14:37 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import Footer from '@/components/Footer'; // Descomente esta linha
 import './globals.css';
 import '../responsive.css';
+import '../imagens-responsivas.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
