8ad40f645f1c2427ea4689ff67cb249fb4bc1eb1
denerval1111 <denerval1111@gmail.com>
2025-05-31 04:09:50 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Header from '@/components/Header';
 import Footer from '@/components/Footer'; // Descomente esta linha
 import './globals.css';
 import '../responsive.css';
